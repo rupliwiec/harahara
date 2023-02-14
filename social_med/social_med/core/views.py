2f57e796604ba1046ea2f4e241c6a420ce5180a0
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-02-14 15:59:20 +0300

    Edit def signup

--- social_med/social_med/core/views.py
+++ social_med/social_med/core/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Is Already Taken')
+                return redirect(signup)
+        else:
+            messages.info(request, 'Password Does Not Match')
+            return redirect(signup)
+
+    else:
+        return render(request, 'signup.html')
