d6310f05e399c1c18ad655f5918ed0e464b6d0fe
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-02-15 19:31:53 +0300

    signin function and chenges to signin.html

--- social_med/social_med/core/views.py
+++ social_med/social_med/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from .models import Profile
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -31,9 +32,13 @@ def signup(request):
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
+                return redirect('signup')
         else:
             messages.info(request, 'Password Does Not Match')
             return redirect('signup')
 
     else:
         return render(request, 'signup.html')
+
+def signin(request):
+    return render(request, 'signin.html')
