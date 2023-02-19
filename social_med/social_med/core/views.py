e08aa62ef3df1c64584a0ba03eaaba80e5b1f3e7
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-02-19 14:01:49 +0300

    Saving profile settings and configurations

--- social_med/social_med/core/views.py
+++ social_med/social_med/core/views.py
@@ -27,6 +27,17 @@ def settings(request):
             user_profile.location = location
             user_profile.save()
 
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        return redirect('settings')
+
     return render(request, 'setting.html', {'user_profile': user_profile})
 
 def signup(request):
