0d23300509493434c4e3d94999b5662f29c4c43c
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-02-19 15:01:08 +0300

    def upload

--- social_med/social_med/core/views.py
+++ social_med/social_med/core/views.py
@@ -10,6 +10,10 @@ from .models import Profile
 @login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
+    
+@login_required(login_url='signin')
+def upload(request):
+    return HttpResponse('<h1>Upload View</h1>')
 
 @login_required(login_url='signin')
 def settings(request):
