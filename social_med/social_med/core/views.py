6f7d54da802a1b0d2e787f8d401d13e0266f5e02
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-02-22 14:51:38 +0300

    Follow and unfollow function

--- social_med/social_med/core/views.py
+++ social_med/social_med/core/views.py
@@ -72,7 +72,13 @@ def follow(request):
         user = request.POST['user']
 
         if FollowersCount.objects.filter(follower=follower, user=user).first():
-            pass
+            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
+            delete_follower.delete()
+            return redirect('/profile/'+user)
+        else:
+            new_follower = FollowersCount.objects.create(follower=follower, user=user)
+            new_follower.save()
+            return redirect('/profile/'+user)
     else:
         return redirect('/')
 
